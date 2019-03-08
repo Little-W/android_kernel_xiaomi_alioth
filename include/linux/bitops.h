db1a078b65a95244fc6976e183d5644f9717c26f
Vineet Gupta <vineet.gupta1@synopsys.com>
2019-03-07 16:28:14 -0800

    include/linux/bitops.h: set_mask_bits() to return old value
    | > Also, set_mask_bits is used in fs quite a bit and we can possibly come up
    | > with a generic llsc based implementation (w/o the cmpxchg loop)
    |
    | May I also suggest changing the return value of set_mask_bits() to old.
    |
    | You can compute the new value given old, but you cannot compute the old
    | value given new, therefore old is the better return value. Also, no
    | current user seems to use the return value, so changing it is without
    | risk.
    
    Link: http://lkml.kernel.org/g/20150807110955.GH16853@twins.programming.kicks-ass.net
    Link: http://lkml.kernel.org/r/1548275584-18096-4-git-send-email-vgupta@synopsys.com
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>
    Suggested-by: Peter Zijlstra <peterz@infradead.org>
    Reviewed-by: Anthony Yznaga <anthony.yznaga@oracle.com>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Cc: Miklos Szeredi <mszeredi@redhat.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Jani Nikula <jani.nikula@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: atndko <z1281552865@gmail.com>
    Signed-off-by: Little-W <1405481963@qq.com>

--- include/linux/bitops.h
+++ include/linux/bitops.h
@@ -254,7 +254,7 @@ static __always_inline void __assign_bit(long nr, volatile unsigned long *addr,
 		new__ = (old__ & ~mask__) | bits__;		\
 	} while (cmpxchg(ptr, old__, new__) != old__);		\
 								\
-	new__;							\
+	old__;							\
 })
 #endif
 
