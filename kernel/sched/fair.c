fbf506ed6e366fef0a971a6e2d5e4f73f2604378
Viresh Kumar <viresh.kumar@linaro.org>
2020-01-08 13:57:04 +0530

    UPSTREAM: sched/fair: Load balance aggressively for SCHED_IDLE CPUs
    The fair scheduler performs periodic load balance on every CPU to check
    if it can pull some tasks from other busy CPUs. The duration of this
    periodic load balance is set to sd->balance_interval for the idle CPUs
    and is calculated by multiplying the sd->balance_interval with the
    sd->busy_factor (set to 32 by default) for the busy CPUs. The
    multiplication is done for busy CPUs to avoid doing load balance too
    often and rather spend more time executing actual task. While that is
    the right thing to do for the CPUs busy with SCHED_OTHER or SCHED_BATCH
    tasks, it may not be the optimal thing for CPUs running only SCHED_IDLE
    tasks.
    
    With the recent enhancements in the fair scheduler around SCHED_IDLE
    CPUs, we now prefer to enqueue a newly-woken task to a SCHED_IDLE
    CPU instead of other busy or idle CPUs. The same reasoning should be
    applied to the load balancer as well to make it migrate tasks more
    aggressively to a SCHED_IDLE CPU, as that will reduce the scheduling
    latency of the migrated (SCHED_OTHER) tasks.
    
    This patch makes minimal changes to the fair scheduler to do the next
    load balance soon after the last non SCHED_IDLE task is dequeued from a
    runqueue, i.e. making the CPU SCHED_IDLE. Also the sd->busy_factor is
    ignored while calculating the balance_interval for such CPUs. This is
    done to avoid delaying the periodic load balance by few hundred
    milliseconds for SCHED_IDLE CPUs.
    
    This is tested on ARM64 Hikey620 platform (octa-core) with the help of
    rt-app and it is verified, using kernel traces, that the newly
    SCHED_IDLE CPU does load balancing shortly after it becomes SCHED_IDLE
    and pulls tasks from other busy CPUs.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/e485827eb8fe7db0943d6f3f6e0f5a4a70272781.1578471925.git.viresh.kumar@linaro.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: mawrick26 <mawrick26@gmail.com>
    Signed-off-by: Little-W <1405481963@qq.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5562,6 +5562,18 @@ static inline void update_overutilized_status(struct rq *rq)
 static inline void update_overutilized_status(struct rq *rq) { }
 #endif
 
+/* Runqueue only has SCHED_IDLE tasks enqueued */
+static int sched_idle_rq(struct rq *rq)
+{
+	return unlikely(rq->nr_running == rq->cfs.idle_h_nr_running &&
+			rq->nr_running);
+}
+
+static int sched_idle_cpu(int cpu)
+{
+	return sched_idle_rq(cpu_rq(cpu));
+}
+
 /*
  * The enqueue_task method is called before nr_running is
  * increased. Here we update the fair scheduling stats and
@@ -5707,6 +5719,7 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	struct sched_entity *se = &p->se;
 	int task_sleep = flags & DEQUEUE_SLEEP;
 	int idle_h_nr_running = idle_policy(p->policy);
+	bool was_sched_idle = sched_idle_rq(rq);
 
 	/*
 	 * The code below (indirectly) updates schedutil which looks at
@@ -5764,6 +5777,10 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		sub_nr_running(rq, 1);
 		dec_rq_walt_stats(rq, p);
 	}
+
+	/* balance early to pull high priority tasks */
+	if (unlikely(!was_sched_idle && sched_idle_rq(rq)))
+		rq->next_balance = jiffies;
 
 	util_est_dequeue(&rq->cfs, p, task_sleep);
 	hrtick_update(rq);
@@ -11423,6 +11440,7 @@ static void rebalance_domains(struct rq *rq, enum cpu_idle_type idle)
 {
 	int continue_balancing = 1;
 	int cpu = rq->cpu;
+	int busy = idle != CPU_IDLE && !sched_idle_cpu(cpu);
 	unsigned long interval;
 	struct sched_domain *sd;
 	/* Earliest time when we have to do rebalance again */
@@ -11462,7 +11480,7 @@ static void rebalance_domains(struct rq *rq, enum cpu_idle_type idle)
 			break;
 		}
 
-		interval = get_sd_balance_interval(sd, idle != CPU_IDLE);
+		interval = get_sd_balance_interval(sd, busy);
 
 		need_serialize = sd->flags & SD_SERIALIZE;
 		if (need_serialize) {
@@ -11478,9 +11496,10 @@ static void rebalance_domains(struct rq *rq, enum cpu_idle_type idle)
 				 * state even if we migrated tasks. Update it.
 				 */
 				idle = idle_cpu(cpu) ? CPU_IDLE : CPU_NOT_IDLE;
+				busy = idle != CPU_IDLE && !sched_idle_cpu(cpu);
 			}
 			sd->last_balance = jiffies;
-			interval = get_sd_balance_interval(sd, idle != CPU_IDLE);
+			interval = get_sd_balance_interval(sd, busy);
 		}
 		if (need_serialize)
 			spin_unlock(&balancing);
