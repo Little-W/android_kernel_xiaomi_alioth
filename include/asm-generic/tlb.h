c90cfc544969927088a1f65c2797bc21aeaca8db
Peter Zijlstra <peterz@infradead.org>
2020-02-03 17:36:53 -0800

    asm-generic/tlb: avoid potential double flush
    Aneesh reported that:
    
    	tlb_flush_mmu()
    	  tlb_flush_mmu_tlbonly()
    	    tlb_flush()			<-- #1
    	  tlb_flush_mmu_free()
    	    tlb_table_flush()
    	      tlb_table_invalidate()
    		tlb_flush_mmu_tlbonly()
    		  tlb_flush()		<-- #2
    
    does two TLBIs when tlb->fullmm, because __tlb_reset_range() will not
    clear tlb->end in that case.
    
    Observe that any caller to __tlb_adjust_range() also sets at least one of
    the tlb->freed_tables || tlb->cleared_p* bits, and those are
    unconditionally cleared by __tlb_reset_range().
    
    Change the condition for actually issuing TLBI to having one of those bits
    set, as opposed to having tlb->end != 0.
    
    Link: http://lkml.kernel.org/r/20200116064531.483522-4-aneesh.kumar@linux.ibm.com
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Reported-by: "Aneesh Kumar K.V" <aneesh.kumar@linux.ibm.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: Little-W <1405481963@qq.com>

--- include/asm-generic/tlb.h
+++ include/asm-generic/tlb.h
@@ -288,7 +288,12 @@ static inline void __tlb_reset_range(struct mmu_gather *tlb)
 
 static inline void tlb_flush_mmu_tlbonly(struct mmu_gather *tlb)
 {
-	if (!tlb->end)
+	/*
+	 * Anything calling __tlb_adjust_range() also sets at least one of
+	 * these bits.
+	 */
+	if (!(tlb->freed_tables || tlb->cleared_ptes || tlb->cleared_pmds ||
+	      tlb->cleared_puds || tlb->cleared_p4ds))
 		return;
 
 	tlb_flush(tlb);
