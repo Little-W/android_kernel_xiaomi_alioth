1dc5bc73f055c67d519c552c0887b749f4808869
LenHackintosh <1403727621@qq.com>
2020-03-15 11:48:22 +0800

    cpu-boost: set int dynamic_stune_boost to 15
    Signed-off-by: Little-W <1405481963@qq.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -75,7 +75,7 @@ cpu_boost_attr_rw(sched_boost_on_powerkey_input);
 static bool sched_boost_active;
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
-static int dynamic_stune_boost;
+static int dynamic_stune_boost = 15;
 module_param(dynamic_stune_boost, uint, 0644);
 static bool stune_boost_active;
 static int boost_slot;
