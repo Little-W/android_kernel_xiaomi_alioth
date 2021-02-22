93f165e1311c1c87c5475f961998830f853c0e60
Sultan Alsawaf <sultan@kerneltoast.com>
2021-02-22 00:43:24 -0800

    scsi: ufs: Add simple IRQ-affined PM QoS operations
    Qualcomm's PM QoS solution suffers from a number of issues: applying
    PM QoS to all CPUs, convoluted spaghetti code that wastes CPU cycles,
    and keeping PM QoS applied for 10 ms after all requests finish
    processing.
    
    This implements a simple IRQ-affined PM QoS mechanism for each UFS
    adapter which uses atomics to elide locking, and enqueues a worker to
    apply PM QoS to the target CPU as soon as a command request is issued.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: I67b8c8246d62317eae3af0c32b010ec579d65d82
    Signed-off-by: Little-W <1405481963@qq.com>

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -61,6 +61,7 @@
 #include <linux/reset.h>
 #include <linux/extcon-provider.h>
 #include <linux/devfreq.h>
+#include <linux/pm_qos.h>
 #include "unipro.h"
 
 #include <asm/irq.h>
@@ -1154,6 +1155,15 @@ struct ufs_hba {
 	union ufs_crypto_cap_entry *crypto_cap_array;
 	u32 crypto_cfg_register;
 	struct keyslot_manager *ksm;
+	struct {
+		struct pm_qos_request req;
+		struct work_struct get_work;
+		struct work_struct put_work;
+		struct mutex lock;
+		atomic_t count;
+		bool active;
+	} pm_qos;
+
 #endif /* CONFIG_SCSI_UFS_CRYPTO */
 
 	ANDROID_KABI_RESERVE(1);
