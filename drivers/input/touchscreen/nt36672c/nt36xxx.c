e8334f0406c93d052e1a010eae13011811413734
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-05-18 22:41:25 -0500

    drivers: cpuidle: remove lpm_disable_for_dev
    * There NO reason to have this function active on kernel other
      than cause significative interruptions on cpuidle
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Signed-off-by: Little-W <1405481963@qq.com>

--- drivers/input/touchscreen/nt36672c/nt36xxx.c
+++ drivers/input/touchscreen/nt36672c/nt36xxx.c
@@ -96,8 +96,6 @@ static int32_t nvt_ts_resume(struct device *dev);
 extern int dsi_panel_lockdown_info_read(unsigned char *plockdowninfo);
 extern void dsi_panel_doubleclick_enable(bool on);
 static int32_t nvt_check_palm(uint8_t input_id, uint8_t *data);
-extern void touch_irq_boost(void);
-extern void lpm_disable_for_dev(bool on, char event_dev);
 extern void xiaomi_touch_send_btn_tap_key(int status);
 uint32_t ENG_RST_ADDR  = 0x7FFF80;
 uint32_t SWRST_N8_ADDR = 0; /* read from dtsi */
