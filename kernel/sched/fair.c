aa5b41882dfdf4a558be0e2703fceb76394423b9
Cyber Knight <cyberknight755@gmail.com>
2021-12-07 22:56:05 +0000

    sched/fair: Disable sched_tunable_scaling when kprofiles is enabled
    We don't need this to be enabled when we tweak other tunables.
    
    Taken from [1].
    
    [1]: https://github.com/tytydraco/Ktweak

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -24,6 +24,8 @@
 
 #include <trace/events/sched.h>
 
+#include <linux/kprofiles.h>
+
 #include "walt.h"
 
 #ifdef CONFIG_SMP
@@ -226,6 +228,12 @@ static unsigned int get_update_sysctl_factor(void)
 {
 	unsigned int cpus = min_t(unsigned int, num_online_cpus(), 8);
 	unsigned int factor;
+	
+#ifdef CONFIG_KPROFILES
+	/* Disable `sched_tunable_scaling` when kprofiles is enabled */
+	if (kp_active_mode() != 0)
+	  sysctl_sched_tunable_scaling = 0;
+#endif
 
 	switch (sysctl_sched_tunable_scaling) {
 	case SCHED_TUNABLESCALING_NONE:
