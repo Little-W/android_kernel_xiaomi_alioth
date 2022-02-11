79410bb954646849b2256cdeef7059535249a07a
EmanuelCN <emanuelghub@gmail.com>
2022-02-11 16:16:53 +0200

    kernel: Boost CPU at zygote-forks
    Signed-off-by: Little-W <1405481963@qq.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -95,7 +95,8 @@
 #include <linux/thread_info.h>
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
-
+#include <linux/devfreq_boost.h>
+#include <linux/cpu_input_boost.h>
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
 #include <linux/uaccess.h>
@@ -2349,7 +2350,8 @@ long _do_fork(unsigned long clone_flags,
 	long nr;
 
 	if (task_is_zygote(current)) {
-		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW_BOOST_FREQ, 50);
+                cpu_input_boost_kick_max(150);
+		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
 	}
 
 	/*
