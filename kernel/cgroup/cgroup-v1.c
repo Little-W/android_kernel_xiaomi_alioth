88e776b30e31097e97cf276d56221a7e14ef0c38
Little-W <1405481963@qq.com>
2022-04-25 19:24:58 +0800

    Revert "kernel: Lower boost durations"
    This reverts commit 34973492a318a96659465d35c5c77db6eabd029a.
    
    Signed-off-by: Little-W <1405481963@qq.com>

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -542,8 +542,6 @@ static ssize_t __cgroup1_procs_write(struct kernfs_open_file *of,
 		goto out_finish;
 
 	ret = cgroup_attach_task(cgrp, task, threadgroup);
-                cpu_input_boost_kick_max(500);
-                devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 500);
 
 	/* This covers boosting for app launches and app transitions */
         if (!ret && !threadgroup &&
