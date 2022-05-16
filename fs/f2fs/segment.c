3a41726a7c7f451d55cbc128d01f4d2d6eadaea8
EmanuelCN <emanuelghub@gmail.com>
2022-05-16 07:44:10 +0000

    Revert "f2fs: don't grab superblock freeze for flush/ckpt thread"
    This reverts commit ac8efe59527e177e93b9c7045389105712c17eca.
    
    Signed-off-by: Little-W <1405481963@qq.com>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -606,6 +606,8 @@ repeat:
 	if (kthread_should_stop())
 		return 0;
 
+	sb_start_intwrite(sbi->sb);
+
 	if (!llist_empty(&fcc->issue_list)) {
 		struct flush_cmd *cmd, *next;
 		int ret;
@@ -625,6 +627,8 @@ repeat:
 		}
 		fcc->dispatch_list = NULL;
 	}
+
+	sb_end_intwrite(sbi->sb);
 
 	wait_event_interruptible(*q,
 		kthread_should_stop() || !llist_empty(&fcc->issue_list));
