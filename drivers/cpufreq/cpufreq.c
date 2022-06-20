5acfb12ac2edd98fb269db5e2bdf1d0357bf6966
Little-W <1405481963@qq.com>
2022-06-21 04:09:46 +0800

    cpufreq: Kill userspace CPU boosting if KProfiles is set to 1
    - We don't need userspace CPU boosting on KProfiles mode 1.
    
    Co-author: Cyber Knight <cyberknight755@gmail.com>
    
    Signed-off-by: Little-W <1405481963@qq.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -32,7 +32,7 @@
 #include <linux/tick.h>
 #include <linux/sched/topology.h>
 #include <linux/sched/sysctl.h>
-
+#include <linux/kprofiles.h>
 #include <trace/events/power.h>
 
 static LIST_HEAD(cpufreq_policy_list);
@@ -709,6 +709,7 @@ static ssize_t show_scaling_cur_freq(struct cpufreq_policy *policy, char *buf)
 	return ret;
 }
 
+
 static int cpufreq_set_policy(struct cpufreq_policy *policy,
 				struct cpufreq_policy *new_policy);
 
@@ -722,7 +723,7 @@ static ssize_t store_##file_name					\
 	int ret, temp;							\
 	struct cpufreq_policy new_policy;				\
 									\
-	if (&policy->object == &policy->min)				\
+	if (kp_active_mode() == 1)					\
 		return count;						\
 									\
 	memcpy(&new_policy, policy, sizeof(*policy));			\
