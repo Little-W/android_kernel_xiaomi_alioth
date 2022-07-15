9cef4811db4ac75b120ec88908d158d2c3f39927
Little-W <1405481963@qq.com>
2022-07-15 09:13:14 -0400

    [kprofiles] cpu_input_boost: set min freq on all but battery
    In many usages people would avoid using battery mode on, this actually
    improves efficiency. So keep it for all.
    
    Amends d997e6dc614b5989e22c4aaa66ae2624a20b0cb5
    
    Signed-off-by: Little-W <1405481963@qq.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -14,6 +14,7 @@
 #include <linux/slab.h>
 #include <linux/version.h>
 #include <linux/sched.h>
+#include <linux/kprofiles.h>
 
 /* The sched_param struct is located elsewhere in newer kernels */
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)
@@ -129,16 +130,20 @@ static unsigned int get_max_boost_freq(struct cpufreq_policy *policy)
 		return min(freq, policy->max);
 }
 
+
 static unsigned int get_min_freq(struct cpufreq_policy *policy)
 {
 	unsigned int freq;
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
 		freq = cpu_freq_min_little;
-	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
-		freq = cpu_freq_min_big;
-        else
-                freq = cpu_freq_min_prime;
+	if (kp_active_mode() != 1) {
+		if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
+			freq = cpu_freq_min_big;
+		else if (cpumask_test_cpu(policy->cpu, cpu_prime_mask))
+			freq = cpu_freq_min_prime;
+	}
+	
 	return max(freq, policy->cpuinfo.min_freq);
 }
 
