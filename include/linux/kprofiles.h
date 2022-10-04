449ea0c72a356c675eeb03eb649ba88435ce4e91
Little-W <1405481963@qq.com>
2022-10-04 12:16:57 +0800

    kprofiles:update to upstream
    Merge 42bcba28ed8caa8f73f5187a99229d47b62729a1 of https://github.com/dakkshesh07/Kprofiles

--- include/linux/kprofiles.h
+++ include/linux/kprofiles.h
@@ -7,22 +7,30 @@
 #define _KPROFILES_H_
 
 #include <linux/types.h>
+#include <linux/delay.h>
 
 #ifdef CONFIG_KPROFILES
-void kprofiles_set_mode_rollback(unsigned int level, unsigned int duration_ms);
-void kprofiles_set_mode(unsigned int level);
-unsigned int active_mode(void);
+void kp_set_mode_rollback(unsigned int level, unsigned int duration_ms);
+void kp_set_mode(unsigned int level);
+
+static bool auto_kprofiles __read_mostly = true;
+static unsigned int kp_mode = CONFIG_DEFAULT_KP_MODE;
+int kp_active_mode(void);
 #else
-static inline void kprofiles_set_mode_rollback(unsigned int level, unsigned int duration_ms)
+static inline void kp_set_mode_rollback(unsigned int level, unsigned int duration_ms)
 {
 }
-static inline void kprofiles_set_mode(unsigned int level)
+static inline void kp_set_mode(unsigned int level)
 {
 }
-static inline unsigned int active_mode(void)
+static inline int kp_active_mode(void)
 {
 	return 0;
 }
+#endif
+
+#ifdef CONFIG_AUTO_KPROFILES
+static bool screen_on = true;
 #endif
 
 #endif /* _KPROFILES_H_ */ 
