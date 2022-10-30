cc8ce955f747981d55a0ad71f4641d473da4eda8
Little-W <1405481963@qq.com>
2022-10-30 11:38:00 +0800

    schedutil:don't limit lp core frequency
    Signed-off-by: Little-W <1405481963@qq.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -382,7 +382,8 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 #else
 	freq = map_util_freq(util, freq, max);
 #endif
-	do_freq_limit(sg_policy, &freq, time);
+	if(!cpumask_test_cpu(sg_policy->policy->cpu, cpu_lp_mask))
+		do_freq_limit(sg_policy, &freq, time);
 	if (freq == sg_policy->cached_raw_freq && !sg_policy->need_freq_update)
 		return sg_policy->next_freq;
 
