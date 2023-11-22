147219f8d60daa6e8fd84f2af52c467e0ceab08d
Yusen <1405481963@qq.com>
2023-11-22 12:38:11 +0800

    Revert "f2fs: Move to late_initcall"
    This reverts commit 446c119c104bb3424aec6d900cfaa1907e9eafeb.

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -4655,4 +4655,10 @@ static void __exit exit_f2fs_fs(void)
 	destroy_inodecache();
 }
 
-late_initcall(init_f2fs_fs);
+module_init(init_f2fs_fs)
+module_exit(exit_f2fs_fs)
+
+MODULE_AUTHOR("Samsung Electronics's Praesto Team");
+MODULE_DESCRIPTION("Flash Friendly File System");
+MODULE_LICENSE("GPL");
+MODULE_SOFTDEP("pre: crc32");
