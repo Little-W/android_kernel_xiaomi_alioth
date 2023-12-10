e32362fd30e0d10a70c3fe75809ef629f24ccd9f
Yusen <1405481963@qq.com>
2023-12-10 16:08:14 +0800

    hwui_mon: change ui_frame_time to int

--- include/linux/hwui_mon.h
+++ include/linux/hwui_mon.h
@@ -8,7 +8,7 @@
 #include <linux/list.h>
 
 typedef void (*hwui_frame_handler)
-        (unsigned int ui_frame_time, ktime_t cur_time);
+        (int ui_frame_time, ktime_t cur_time);
 
 struct hwui_mon_receiver {
 	struct list_head list;
